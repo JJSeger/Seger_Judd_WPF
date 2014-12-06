5ed2f0ea9a293f964d30ec2024cc60380d2e4a81
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 16:45:09 -0500

    playing with the if else statement
    playing with the if else statement

--- Expressions_Personal/js/script1.js
+++ Expressions_Personal/js/script1.js
@@ -1,9 +1,11 @@
 /**
  * Created by JuddHome on 12/6/14.
  */
+
+
 var glassDay = 8;
 
-var dailyWater;
+
 
 
 
@@ -11,18 +13,19 @@ var name = prompt("What is your name?");
 
 var water = prompt("Hello " + name ,"How many glasses of water do you drink daily?");
 
-var moreWater = water - glassDay;
+var moreWater = glassDay - water;
+
+
 
     if(water >= glassDay){
 
-        "Congrats! You are drinking enough water daily! "
+           "Congrats! You are drinking enough water daily! "
 
                         }
+    else(water < glassDay){
 
-            else(water < glassDay)
-                    {
 
-                "You should be drinking" + moreWater, " more glasses of water per day!"
+         "You should be drinking" + moreWater ," more glasses of water per day!"
 
 
 
