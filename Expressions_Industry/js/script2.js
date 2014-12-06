95035a47dd49ebfb159a4a664767dbda8b8148e6
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 18:26:23 -0500

    added date to script.js file
    added date to script.js file

--- Expressions_Industry/js/script2.js
+++ Expressions_Industry/js/script2.js
@@ -1,5 +1,5 @@
 //This a GPA calculator that could be found on a college website
-//Judd Seger Week 2 Assignment Expressions_Industry
+//Judd Seger Week 2 12/06/14 Assignment Expressions_Industry
 
 
 
