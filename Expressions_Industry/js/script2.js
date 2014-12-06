31a1cdb345847b5f7005fcb7a36559f99a11bf52
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 18:59:35 -0500

    console.log display
    console.log display of 7 holidays, 2 weeks vacation and 5 sick days
    included in this equation

--- Expressions_Industry/js/script2.js
+++ Expressions_Industry/js/script2.js
@@ -14,3 +14,8 @@ var assocCost = targetSalary * .30;
 
 //Displays the result of the associated costs
 console.log("Your associated costs are based \n on thirty percent of your salary \n which is equal to " + assocCost);
+
+
+//Displays the fact that there are 7 holidays, 2 weeks vacation, and 5 sick days included in this equation
+console.log("We are going to also calculate in \n 7 legal holidays - 56 hours \n 2 weeks vacation - 80 hours" +
+" \n and 5 sick days - 40 hours \n for a grand total of \n 176 hours ");
