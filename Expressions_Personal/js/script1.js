950c51b73c84780b7441038ab5b2678a394ef755
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 16:49:18 -0500

    removed the If else statement
    removed the If else statement

--- Expressions_Personal/js/script1.js
+++ Expressions_Personal/js/script1.js
@@ -3,7 +3,7 @@
  */
 
 
-var glassDay = 8;
+
 
 
 
@@ -13,21 +13,3 @@ var name = prompt("What is your name?");
 
 var water = prompt("Hello " + name ,"How many glasses of water do you drink daily?");
 
-var moreWater = glassDay - water;
-
-
-
-    if(water >= glassDay){
-
-           "Congrats! You are drinking enough water daily! "
-
-                        }
-    else(water < glassDay){
-
-
-         "You should be drinking" + moreWater ," more glasses of water per day!"
-
-
-
-
-}
