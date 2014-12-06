5d37427c026d5249f041026b2b9877324d6a5b80
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 18:50:21 -0500

    created var assocCost that calculates the associated costs
    created var assocCost that calculates the associated costs by
    calculating 30 percent of target salary and displays to the console the
    associated costs

--- Expressions_Industry/js/script2.js
+++ Expressions_Industry/js/script2.js
@@ -6,5 +6,11 @@
 //Prompting user for their target salary
 var targetSalary = prompt("What is your target salary?");
 
-//
 
+//calculates the associated costs by multiplying the target salary by 30%
+var assocCost = targetSalary * .30;
+
+
+
+//Displays the result of the associated costs
+console.log("Your associated costs are based \n on thirty percent of your salary \n which is equal to " + assocCost);
