980e8c4e0224fc1d775ba68e248b3497fb0f8e75
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 19:38:28 -0500

    created the actual hours alert
    created the actual hours alert

--- Expressions_Industry/js/script2.js
+++ Expressions_Industry/js/script2.js
@@ -10,14 +10,14 @@ var typicalYearlyHours = 2080;
 
 
 //declared var billableHours and assigned value
-var billableHours = otherDaysOff - typicalYearlyHours;
+var billableHours = otherDaysOff - typicalYearlyHours; //(First Calculation)
 
 //Prompting user for their target salary
 var targetSalary = prompt("What is your target salary?");
 
 
 //calculates the associated costs by multiplying the target salary by 30%
-var assocCost = targetSalary * .30;
+var assocCost = targetSalary * .30; //second calculation
 
 
 
@@ -30,11 +30,16 @@ console.log("We are going to also calculate in \n 7 legal holidays - 56 hours \n
 " \n and 5 sick days - 40 hours \n for a grand total of \n 176 hours ");
 
 //created an alert that notifies the user what their total billable hours are
-alert ("The grand total of your billable hours is" + billableHours);
+alert("Your billable hours are" + billableHours);
 
 // Creating Alert to notify the user that their are some additional deductions from billable hours
-alert ("There are also some hours spend in the office doing other things\n these are not billable hours either.");
+alert("There are also some hours spend in the office doing other things\n these are not billable hours either.");
 
 
 // prompt user to enter addition hours that need to be deducted from the billable hours
 var hourDed = prompt("Out of the total billable hours," + billableHours + ",\nhow many are spent doing other things?");
+
+
+
+
+alert("Now this is more like it, \n these are the actual hours you can bill for. ")
