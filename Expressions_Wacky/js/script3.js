7d0c522106186f1beb3aa9142feb534d7f2bcf8a
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 20:54:02 -0500

    deleted comment and replaced
    deleted comment and replaced

---
+++ Expressions_Wacky/js/script3.js
@@ -0,0 +1,10 @@
+// This is a Wacky Calculator that is going to calculate farts
+//Judd Seger- Week 2 Assignment - Expressions_Wacky
+
+
+// created initial warning alert
+alert("WARNING THE FOLLOWING MIGHT\n SEEM TO BE A LITTLE WEIRD!!! ")
+
+
+
+var fart = prompt()
