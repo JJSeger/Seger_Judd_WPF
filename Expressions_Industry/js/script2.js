c59cccb26565b223080711ad724bc6f2cad9ef5a
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 19:56:28 -0500

    moved the second console.log
    moved the second console.log so that it displays after the total
    billable hours are displayed

--- Expressions_Industry/js/script2.js
+++ Expressions_Industry/js/script2.js
@@ -25,12 +25,16 @@ var assocCost = targetSalary * .30; //second calculation
 console.log("Your associated costs are based \n on thirty percent of your salary \n which is equal to " + assocCost);
 
 
+
+
+//created an alert that notifies the user what their total billable hours are
+alert("Your billable hours are" + billableHours);
+
+
 //Displays the fact that there are 7 holidays, 2 weeks vacation, and 5 sick days included in this equation
 console.log("We are going to also calculate in \n 7 legal holidays - 56 hours \n 2 weeks vacation - 80 hours" +
 " \n and 5 sick days - 40 hours \n for a grand total of \n 176 hours ");
 
-//created an alert that notifies the user what their total billable hours are
-alert("Your billable hours are" + billableHours);
 
 // Creating Alert to notify the user that their are some additional deductions from billable hours
 alert("There are also some hours spend in the office doing other things\n these are not billable hours either.");
@@ -49,7 +53,7 @@ var actHours = weekHour - billableHours;
 
 
 //actual hours alert that displays the total billable hours minus extra hours spent in the office
-alert("Now this is more like it!\n " + actHours + "hours\n, these are the actual hours you can bill for. ");
+alert("Now this is more like it!\n " + actHours + "hours, \nthese are the actual hours you can bill for. ");
 
 
 
