c37e78af24274d73b7c5252138377fff816471da
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 19:13:37 -0500

    created variables otherDaysOff, typicalYearlyHours and billableHours
    created variables otherDaysOff, typicalYearlyHours and billableHours
    and assigned value

--- Expressions_Industry/js/script2.js
+++ Expressions_Industry/js/script2.js
@@ -1,9 +1,16 @@
 //freelance rates calculator for web design and development
 //Judd Seger Week 2 12/06/14 Assignment Expressions_Industry
-// created variable otherDaysOff 
+
+
+// created variable otherDaysOff and assigned value
 var otherDaysOff = 176;
 
+//created variable typicalYearlyHours and assigned value
+var typicalYearlyHours = 2080;
 
+
+//declared var billableHours and assigned value
+var billableHours = otherDaysOff - typicalYearlyHours;
 
 //Prompting user for their target salary
 var targetSalary = prompt("What is your target salary?");
@@ -23,4 +30,4 @@ console.log("We are going to also calculate in \n 7 legal holidays - 56 hours \n
 " \n and 5 sick days - 40 hours \n for a grand total of \n 176 hours ");
 
 
-alert ()
+alert ("The grand total of your billable hours is  )
