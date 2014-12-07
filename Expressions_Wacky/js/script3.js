5b13103d29fddb00248a3eda2834d5956aab17db
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 21:07:51 -0500

    displaying to the console that we need to multiply by 24
    displaying to the console that we need to multiply by 24

--- Expressions_Wacky/js/script3.js
+++ Expressions_Wacky/js/script3.js
@@ -14,5 +14,14 @@ var fart = prompt("Have you ever wondered how\n many times you pass gas during t
 alert(+ fart " Good Because We Have Too!");
 
 
-//prompting the user to enter howmay times per hour they pass gas
-var bigFart = prompt("Now we obviously have to ask...\n  how many times per hour do you ....\n  Float a rotten apple?");
+//prompting the user to enter how many times per hour they pass gas
+var bigFart = prompt("Now we obviously have to ask...\n  how many times per hour do you ....\n  Float the old rotten apple?");
+
+//creating a variable dayFart to store the amount of times per day that the user breaks wind
+var dayFart = bigFart * 24;
+
+
+
+// displaying to the console that we need to multiply by 24
+console.log("That means that in order for us to calculate\n how many times you burn your britches \nwe need to multiply by 24 hours in a day.");
+
