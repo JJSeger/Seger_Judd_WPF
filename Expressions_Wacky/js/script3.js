fccd08084d861131d361eb203d1965b94a579491
JJSeger <jjsbowhunter@yahoo.com>
2014-12-06 20:59:22 -0500

    Created the Good us too alert
     Created the Good us too alert

--- Expressions_Wacky/js/script3.js
+++ Expressions_Wacky/js/script3.js
@@ -8,3 +8,7 @@ alert("WARNING THE FOLLOWING MIGHT\n SEEM TO BE A LITTLE WEIRD!!! ")
 
 //created initial prompt and assigned it to the variable fart
 var fart = prompt("Have you ever wondered how\n many times you pass gas during the day?");
+
+
+// Created the Good us too alert
+alert(+ fart " Good Because We Have Too!");
