295eaf8a6c97a56472ad092e2f2cd3918896f2cc
JJSeger <jjsbowhunter@yahoo.com>
2014-12-11 21:34:53 -0500

    changing prompt to console.log
    changing prompt to console.log

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -15,7 +15,7 @@ var name = prompt("What is your name?");
 console.log("Hello " + name +" Today we are going to figure out if \nit is time to put gas in your vehicle");
 
 //declaring varible and assigning value of the user input
- gasTank = prompt("Is your gas gauge below 1/4 tank remaining?");
+ gasTank = prompt("What does your gas gauge read?\n Pleas enter:\n 5 - Full\n 4 - 3/4full\n 3 - 1/2full\n 2 - 1/4full\n 1 - Running on fumes");
 
 
 //Creating If Statement to test the value o the variable gasTank
@@ -25,7 +25,7 @@ if(gasTank = fullGasTank) {
     //nested If Statement
     if(gasTank < 5)
     {
-        prompt("What does your gas gauge read?\n Pleas enter:\n 5 - Full\n 4 - 3/4full\n 3 - 1/2full\n 2 - 1/4full\n 1 - Running on fumes");
+       console.log("Sorry but you should consider refueling.")
 
 
     }
