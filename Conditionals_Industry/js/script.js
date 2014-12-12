c649764b12695ac31f5ecc850f350deb359e2fda
JJSeger <jjsbowhunter@yahoo.com>
2014-12-11 23:00:02 -0500

    added variable and prompt for the users name name tests if the field is filled out
    added variable and prompt for the users name name tests if the field is
    filled out

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,3 +1,19 @@
-/**
- * Created by JuddHome on 12/11/14.
- */
+//Judd Seger   12/11/14
+//Week 3 Conditionals_industry assignment
+//What background color would you like on your website
+
+
+
+
+
+//simple greeting that will alert if the field is not filled out
+var name = prompt("What is your name?");
+
+//If statement checking the variable name
+if (name == "" ){
+
+    //the alert that will come up if the name variable does not get a value
+    alert("Sorry but none of this counts without your name!?!");
+
+
+
