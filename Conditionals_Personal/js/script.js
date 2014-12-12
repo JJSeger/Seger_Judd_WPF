e5c7d06d1fb3974456abb9c1962f707716c9ef05
JJSeger <jjsbowhunter@yahoo.com>
2014-12-11 20:11:22 -0500

    changed alert to console.log
    changed alert to console.log

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -11,5 +11,5 @@
 
 var name = prompt("What is your name?");
 
-alert("Hello" + name +"Are you ready to calculate \nhow many gallons of gas you use per mile?");
+console.log("Hello " + name +" Are you ready to calculate \nhow many gallons of gas you use per mile?");
 
