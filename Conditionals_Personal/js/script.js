6c83de8cd8e9cee8bb81b3194c3180f2fc64aaba
JJSeger <jjsbowhunter@yahoo.com>
2014-12-11 21:54:32 -0500

    changing nested if statement to else statement
    changing nested if statement to else statement

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -17,12 +17,11 @@ console.log("Hello " + name +" Today we are going to figure out if \nit is time 
  var gasTank = prompt("What does your gas gauge read?\n Pleas enter:\n 5 - Full\n 4 - 3/4full\n 3 - 1/2full\n 2 - 1/4full\n 1 - Running on fumes");
 
 
-//Creating If Statement to test the value o the variable gasTank
-if(gasTank == fullGasTank) {
+//Creating If / else Statement to test the value of the variable gasTank
+if(gasTank >= 3) {
     console.log("Congrats! Your good to go!");}
 
-    //nested If Statement
-    if(gasTank < 5){
+    //else statement
+   else{
         console.log("Sorry but you should consider refueling.");
-
     }
