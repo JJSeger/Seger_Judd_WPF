e7a4137c8e605cdb81bf67768e9274a95520ae1f
JJSeger <jjsbowhunter@yahoo.com>
2014-12-11 21:43:19 -0500

    adding comments
    adding comments

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,9 +3,8 @@
 //This code decides if it is time to fill up your gas tank
 
 
-
+//variable fullGasTank
 var fullGasTank = 5;
-
 
 
 //Simple greeting
@@ -15,19 +14,15 @@ var name = prompt("What is your name?");
 console.log("Hello " + name +" Today we are going to figure out if \nit is time to put gas in your vehicle");
 
 //declaring varible and assigning value of the user input
- gasTank = prompt("What does your gas gauge read?\n Pleas enter:\n 5 - Full\n 4 - 3/4full\n 3 - 1/2full\n 2 - 1/4full\n 1 - Running on fumes");
+ var gasTank = prompt("What does your gas gauge read?\n Pleas enter:\n 5 - Full\n 4 - 3/4full\n 3 - 1/2full\n 2 - 1/4full\n 1 - Running on fumes");
 
 
 //Creating If Statement to test the value o the variable gasTank
 if(gasTank = fullGasTank) {
-    console.log("Congrats! Your good to go!");
+    console.log("Congrats! Your good to go!");}
 
     //nested If Statement
-    if(gasTank < 5)
-    {
-       console.log("Sorry but you should consider refueling.")
-
+    if(gasTank < 5){
+        console.log("Sorry but you should consider refueling.");
 
     }
-
-}
