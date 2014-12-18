df67f5fefcb7f8a5a8056f1170b59cdb0d410fce
JJSeger <jjsbowhunter@yahoo.com>
2014-12-18 18:30:00 -0500

    adding var feeTime, work and studies
    adding var feeTime, work and studies

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,33 +1,46 @@
 //Judd Seger 12/18/14
 //personal functions assignment
+//measures sleep based on 8 hours per night
 
 
 
 
 
 
+var name = prompt("What is you name?");
+
+
+alert("Hello "+ name + " We are going to figure out together if you get enough sleep!");
+
+var freeTime = prompt("How many hours of free time time \ndo you typically have per day?");
+
+var work = ("How many hours do you spend working per day?");
+
+var studies = ("How many hours do you spend on your studies per day");
 
 
 
-
-
-
-// the function returns the product of p1 and p2
+// the function returns
 
 //function with three parameters
-function myFunction(p1, p2) {
-    return p1 * p2;
-}
 
+
+var sleep = sleepTime(, );        // Function is called, return value will end up in x
+
+function myFunction(a, b) {
+    return a * b;                // Function returns the product of a and b
+}
 
 
 // need an expression with two math operators
 
+var studyTime = work - familyTime;
 
 
 
+//at least one logical operator
 
-
+//  !,&&,||
 
 
 
@@ -47,6 +60,8 @@ var cacArea  = function(width, height){
 
 
 //invocation of  anonymous function
+
+
 
 //calcArea( args, args );
 
