c8df157da4b2fc63935c574751f86c6bfebd9584
JJSeger <jjsbowhunter@yahoo.com>
2014-12-18 21:14:27 -0500

    finishing up and testing code
    finishing up and testing code

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,7 +5,6 @@
 
 
 
-
 //prompting the user to enter their name and storing the value in "name"
 var name = prompt("What is you name?");
 
@@ -13,37 +12,47 @@ var name = prompt("What is you name?");
 //an alert saying helo to the user and stating the intentions
 alert("Hello "+ name + " We are going to figure out together if you get enough sleep!");
 
+//prompting the user to enter the amount of hours they work per day and storing the value in "work"
+var work = ("How many hours do you spend working per day?");
+
+
 
 //prompting the user to enter the amount of free time they have per day and storing the value in "freeTime"
 var freeTime = prompt("How many hours of free time time \ndo you typically have per day?");
 
-//prompting the user to enter the amount of hours they work per day and storing the value in "work"
-var work = ("How many hours do you spend working per day?");
+
 
 //prompting the user to enter the amount of hours they spen on studies per day and storing the value in "studies"
 var studies = ("How many hours do you spend on your studies per day");
 
 
-//if else if statement
+// adds the user input from work and studies
+var grandTotal = work + studies;
 
-if(freeTime > 24){
-    alert("Sorry your free time exceeds the amount of hours in a day!");
+//subtracts the sum of work studies and subtracts it from 24
+var sleepTimeAvailable = grandTotal - 24;
 
-}if(work > 24) {
-    alert("Sorry but your working hours exceed the amount of hours in a day!");
+//an alert giving the total of working hours plus studying hours
+alert("The grand total of your working hours plus your studying hours is " + grandTotal );
 
-}if (studies > 24) {
-    alert("Sorry but your studying hours exceed the amount of hours in a day!");
+//writes to the console the amount of sleep time available
+console.log("That means that the sleep time you have available is equal to" + sleepTimeAvailable);
 
-}if(studies + work + freeTime > 24 ){
+//SleepTime function w/ three parameters and args.
+function sleepTime(4,8,2){
 
-    alert("Sorry but the sum of your work hours, \nstudy hours and your free time \nexceed the amount of hours in a day!");
 
-}else{
-    console.log("Ok now we are going to start playing with numbers...");
+    var justSaying = a + b + c
+
+    console.log ("Hypothethically speaking if you were \nto only sleep 4 hrs. per day, \nwork for 8 hrs, and spend 2 hrs \nstudying you would only have " + justSaying - 24 + " hours \nleft to split between sleep and free time." );
+
+
+
 }
 
+//calling the sleepTime function
 
+sleepTime();
 
 
 // need an expression with two math operators
@@ -51,69 +60,46 @@ if(freeTime > 24){
 console.log("The numbers you have entered are" + freeTime " hours for your free time \n" + work + " hours for your working hours\n and" + studies + " hours for your hours spent studying.");
 
 
-)
 
 
+//function checking the hours in a day compared to the hours worked
 
+function workTime() {
 
+    var workTime = work - 24;
 
+   return  workTime;
 
-
-//This is a one-line shorthand for an if-then statement. It is called a ternary operator or a conditional operator.
-
-  //  Here is an example of code that could be shortened with the conditional operator:
-
-    //if(user is Over 21) {
-      //  serveGrapeJuice();
- //   }
-   // else {
-     //   serveWine();
-    //    }
-
-//This can be shortened with the ?: like so:
-
-
-
-//In Javascript conditional operator can evaluate to an expression, not just a statement:
-
- //   var userType = userIsYoungerThan18 ? "Minor" : "Adult";
-// serveDrink(userIsYoungerThan21 ? "Grape Juice" : "Wine");
-
-
-
-
-//terney operator
-
-//   r = (condition) ? 'something' : 'somethingelse';
-
-
-
-
-
-
-//else if statement
-
-if (condition) {
-    block of code to be executed if the condition is true
-} else {
-    block of code to be executed if the condition is false
 }
 
 
-//function with three parameters
+//calling function
+workTime();
 
 
+//function that adds the value of the variables work + freeTime and subtracts it from the hours in a day
+function studyTime() {
+
+    var studyTime = work + freeTime - 24;
+
+    return  studyTime;
 
 
-
-
-
-         // Function is called, return value will end up in x
-
-function myFunction(a, b) {
-    return a * b;                // Function returns the product of a and b
 }
 
+//calling the studyTime function
+studyTime();
+
+
+
+//function that adds the value of the variables work + studies and subtracts it from the hours in a day
+    function extraTime() {
+
+        var extraTime = work + studies  - 24;
+
+        return extraTime;
+    }
+extraTime();
 
 
 
@@ -121,17 +107,14 @@ function myFunction(a, b) {
 
 
 
-//at least one logical operator
-
-//  !,&&,||
+//ternary operator
 
 
+var totalHours;
+totalHours = (studies + work + freeTime > 24 ) ? 'Total is too high!' : 'You need to adjust your hourly figures.';
 
 
 
-
-//variable declaration
-var workHoursPerDay;
 
 //anonymous function
 workHoursPerDay = function typicalWorkHoursPerDay() {
@@ -150,6 +133,11 @@ workHoursPerDay = function typicalWorkHoursPerDay() {
 
 //invocation of  anonymous function
 typicalWorkHoursPerDay();
+
+
+
+
+
 
 
 
