3d00b01dde994bfd9a652cd298f5d25c1cc25f98
JJSeger <jjsbowhunter@yahoo.com>
2014-12-19 22:57:30 -0500

    var x = mySleepFunction(4, 6, 8);        // Function is called, return value will end up in x
     function mySleepFunction(a, b, c){
    
        return (a + b + c) - 24;
    
    debugging
    
    debugging

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -16,7 +16,7 @@ alert("Hello "+ name + " We are going to figure out together if you get enough s
 var work = prompt("How many hours do you spend working per day?");
 
 //prompting user for the hours of sleep they get
-var sleep = prompt("How many hours of sleep do you typically get per night?");
+prompt("How many hours of sleep do you typically get per night?");
 
 
 //prompting the user to enter the amount of free time they have per day and storing the value in "freeTime"
@@ -28,20 +28,29 @@ var freeTime = prompt("How many hours of free time time \ndo you typically have 
 var studies = prompt("How many hours do you spend on your studies per day");
 
 
-//mySleepFunction
 
-var x = mySleepFunction(4, 6, 8);        // Function is called, return value will end up in x
-
-function mySleepFunction(a, b, c) {
-    return (a + b + c) - 24;                // Function returns the product of a,b and c
+var wo = work;
+var fr = freeTime;
+var st = studies;
 
 
-alert("Hypothetically speaking \n if you were to have two hours of studying \n fours hours of sleep \n and the typical eight hours at work \n " +
-"that would leave you with " + x +" to split up \nbetween sleep and everything else life throws at you " );
 
 
-}
-mySleepFunction();
+myWorkFunction();
+
+studyTimeFunction();
+
+extraTimeFunction();
+
+totalSleepFunction();
+
+
+
+
+
+
+
+
 
 
 
@@ -53,70 +62,57 @@ console.log("The numbers you have entered are " + freeTime + " hours for your fr
 
 //function checking the hours in a day compared to the hours worked
 
-var r = myWorkFunction(work - 24);        // Function is called, return value will end up in r
+var r = myWorkFunction(work );        // Function is called, return value will end up in r
+function myWorkFunction(work) {
+    return (work) - 24;                // Function returns the difference of work - 24
 
-function myWorkFunction() {
-    return r;                // Function returns the difference of work - 24
-
-
-    alert("You have a total of " + r + " hours left for studies, freetime and sleep.");
 
 
 }
-
+alert("You have a total of " + r + " hours left for studies, freetime and sleep.");
 
 
 
 //function that adds the value of the variables work + freeTime and subtracts it from the hours in a day
 
-    var s = studyTimeFunction(work + freeTime - 24); // Function is called, return value will end up in s
+    var s = studyTimeFunction(work, freeTime); // Function is called, return value will end up in s
 
-    function studyTimeFunction() {
+    function studyTimeFunction(work, freeTime) {
 
-    return s;
+    return (work + freeTime) - 24;
 
-        alert("You have a total of " + s + " hours left for to split up between studing and sleeping.")
+
 
 }
 
+alert("You have a total of " + s + " hours left for to split up between studing and sleeping.");
 
-//invoking  the studyTimeFunction function
-studyTimeFunction();
 
 
 //function that adds the value of the variables work + studies and subtracts it from the hours in a day
 
-    var t = extraTimeFunction(work + studies - 24 ); // Function is called, return value will end up in t
+    var t = extraTimeFunction(work, studies ); // Function is called, return value will end up in t
 
-    function extraTimeFunction() {
+    function extraTimeFunction(work, studies) {
 
-        return t;
+        return  (work + studies) - 24;
 
 
-            alert("You have a total of " + t + " hours left to spilt up between sleepping and free time.")
+
         }
 
+alert("You have a total of " + t + " hours left to spilt up between sleeping and free time.");
 
-//invoking the extraTimeFunction
-extraTimeFunction();
+// that adds worktime, freetime, and study time and subtracts it from 24
 
+var z = totalSleepFunction( work, studies , freeTime);
 
-
-var z = sleepTimeFunction( work + studies + freeTime);
-
-function sleepTimeFunction(){
+function totalSleepFunction(work,studies,freeTime){
 
 
-    return z;
-
+    return (work + studies + freeTime);
 
 }
-
-
-
-
-
-
 
 
 
@@ -124,7 +120,13 @@ function sleepTimeFunction(){
 
 //ternary operator
 
-    z > 16 ? 'You need to get more sleep' : 'You get plenty of sleep ';
+z > 16 ? 'You need to get more sleep' : 'You get plenty of sleep ';
+
+
+
+
+
+
 
 
 
